5be9bcebdc796a528d1d8fa4d2cd85ef360a1483
keonchang Lee <rjsckdd12@gmail.com>
2023-07-31 09:23:00 +0900

    init : 프로젝트 설정

---
+++ src/main/java/towardssweetly/datascheduler/DataSchedulerApplication.java
@@ -0,0 +1,13 @@
+package towardssweetly.datascheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/towardssweetly/datascheduler/DataSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package towardssweetly.datascheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
